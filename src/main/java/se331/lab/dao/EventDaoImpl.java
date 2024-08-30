fa087730e2e03c4e7b4c8ec4d2320586b96a05b1
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-08-30 10:24:12 +0700

    5.4

--- src/main/java/se331/lab/dao/EventDaoImpl.java
+++ src/main/java/se331/lab/dao/EventDaoImpl.java
@@ -1,26 +1,18 @@
-package se331.lab.rest.controller;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.server.ResponseStatusException;
-import se331.lab.entity.Event;
+package se331.lab.dao;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import se331.lab.entity.Event;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-public class EventController {
+@Repository
+public class EventDaoImpl implements EventDao {
     List<Event> eventList;
 
     @PostConstruct
     public void init() {
-        eventList = new ArrayList<>();
+        eventList = new ArrayList<Event>();
         eventList.add(Event.builder()
                 .id(123L)
                 .category("animal welfare")
@@ -28,7 +20,7 @@ public class EventController {
                 .description("Find your new feline friend at this event.")
                 .location("Meow Town")
                 .date("January 28, 2022")
-                .time("12:00")
+                .time("12.00")
                 .petAllowed(true)
                 .organizer("Kat Laydee")
                 .build());
@@ -87,42 +79,36 @@ public class EventController {
                 .petAllowed(false)
                 .organizer("Brody Kill")
                 .build());
+
+
     }
-    @GetMapping("events")
-    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage
-            , @RequestParam(value = "_page", required = false) Integer page) {
-        perPage = perPage == null ? eventList.size() : perPage;
-        page = page == null ? 1 : page;
-        Integer firstIndex = (page-1) * perPage;
-        List<Event> output = new ArrayList<>();
-        HttpHeaders responseHeader = new HttpHeaders();
-        responseHeader.set("x-total-count",
-                String.valueOf(eventList.size()));
-        try {
-            for (int i = firstIndex; i < firstIndex + perPage; i++) {
-                output.add(eventList.get(i));
-            }
-            return new ResponseEntity<>(output, responseHeader, HttpStatus.OK);
-        }catch (IndexOutOfBoundsException ex) {
-            return new ResponseEntity<>(null, responseHeader, HttpStatus.OK);
-        }
+    @Override
+    public Integer getEventSize() {
+        return eventList.size();
     }
 
-    @GetMapping("events/{id}")
-    public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
+    @Override
+    public List<Event> getEvents(Integer pageSize, Integer page) {
+        pageSize = pageSize == null ? eventList.size() : pageSize;
+        page = page == null ? 1 : page;
+        Integer firstIndex = (page - 1) * pageSize;
+        List<Event> output = new ArrayList<>();
+        for (int i = firstIndex; i < firstIndex + pageSize; i++) {
+            output.add(eventList.get(i));
+        }
+        return output;
+    }
+
+    @Override
+    public Event getEvent(Long id) {
         Event output = null;
-        for (Event event :
-                eventList) {
+        for (Event event: eventList) {
             if (event.getId().equals(id)) {
                 output = event;
                 break;
             }
         }
-        if (output != null) {
-            return ResponseEntity.ok(output);
-        }else {
-            throw new
-                    ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
-        }
+        return output;
     }
 }
+
